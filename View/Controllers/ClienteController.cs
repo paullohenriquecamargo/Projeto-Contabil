5b9c6d04366058c5215da3e0620dd9391bf55f70
LoeneSilva <49409909+LoeneSilva@users.noreply.github.com>
2019-07-05 17:26:33 -0300

    Desenvolivimento Clientes

--- View/Controllers/ClienteController.cs
+++ View/Controllers/ClienteController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Model;
+using Repository.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,32 @@ namespace View.Controllers
 {
     public class ClienteController : Controller
     {
+        private ClienteRepository repository;
+
+        public ClienteController()
+        {
+            repository = new CategoriaRepository();
+        }
+
         // GET: Cliente
         public ActionResult Index()
         {
+            List<Cliente> clientes = repository.ObterTodos();
+            ViewBag.Cliente = clientes;
             return View();
         }
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public ActionResult Store(string nome)
+        {
+            Cliente cliente = new Cliente();
+            cliente.Nome = nome;
+            return RedirectToAction("Index");
+        }
+
     }
 }
